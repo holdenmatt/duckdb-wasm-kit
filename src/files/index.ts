b6db0c06144d74d988aa6c864f90586698533060
Matt Holden <holden.matt@gmail.com>
2023-07-19 17:12:06 -0600

    Simplify file exports

--- src/files/index.ts
+++ src/files/index.ts
@@ -1,6 +1,3 @@
-import { Arrow, ARROW_MIME_TYPE, arrowToJSON } from "./arrow";
-import { CSV_MIME_TYPE } from "./csv";
-import { PARQUET_MIME_TYPE } from "./parquet";
-
-export { ARROW_MIME_TYPE, arrowToJSON, CSV_MIME_TYPE, PARQUET_MIME_TYPE };
-export type { Arrow };
+export * from "./arrow";
+export * from "./csv";
+export * from "./parquet";
